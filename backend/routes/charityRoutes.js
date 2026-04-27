0278a6b400d5fd19a2c51f6a6ffa0df7c9162905
PRATIK DABHADE <96970200+pratik-71@users.noreply.github.com>
2026-04-28 00:46:59 +0530

    Delete backend directory

--- backend/routes/charityRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const charityController = require('../controllers/charityController');
-
-// Public route to fetch all charities
-router.get('/', charityController.getCharities);
-
-module.exports = router;
